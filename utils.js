66be877e4967cf60558859ec2408e2e5b1419c59
satyajit nayak <satyajitn15@gmail.com>
2024-02-18 19:35:01 +0530

    add a canvas for drawing network structure
    * draw neural network structure.
    * style the neural network.

--- utils.js
+++ utils.js
@@ -42,3 +42,11 @@ function polygonsIntersect(polygon1, polygon2) {
   }
   return false
 }
+
+function getRGBA(value) {
+  const alpha = Math.abs(value)
+  const R = value < 0 ? 0 : 255
+  const G = R
+  const B = value > 0 ? 0 : 255
+  return `rgba(${R},${G},${B},${alpha})`
+}
---
+++ visualizer.js
@@ -0,0 +1,84 @@
+class Visualizer {
+  static #getNodeX(nodes, index, left, right) {
+    return lerp(left, right, nodes.length === 1 ? 0.5 : index / (nodes.length - 1))
+  }
+
+  static drawNetwork(ctx, network) {
+    const margin = 50
+    const left = margin
+    const top = margin
+    const width = ctx.canvas.width - margin * 2
+    const height = ctx.canvas.height - margin * 2
+
+    const levelHeight = height / network.levels.length
+
+    for (let i = network.levels.length - 1; i >= 0; --i) {
+      const levelTop = top + lerp(
+        height - levelHeight,
+        0,
+        network.levels.length === 1 ? 0.5 : i / (network.levels.length - 1)
+      )
+
+      Visualizer.drawLevel(ctx, network.levels[i], left, levelTop, width, levelHeight)
+    }
+  }
+
+  static drawLevel(ctx, level, left, top, width, height) {
+    const right = left + width
+    const bottom = top + height
+
+    const {inputs, outputs, weights, biases} = level
+
+    // connect the neurons
+    for (let i = 0; i < inputs.length; ++i) {
+      for (let j = 0; j < outputs.length; ++j) {
+        ctx.beginPath()
+        ctx.moveTo(Visualizer.#getNodeX(inputs, i, left, right), bottom)
+        ctx.lineTo(Visualizer.#getNodeX(outputs, j, left, right), top)
+        ctx.lineWidth = 2
+        ctx.strokeStyle = getRGBA(weights[i][j])
+        ctx.stroke()
+      }
+    }
+
+    const nodeRadius = 18
+    // draw bottom level of neurons
+    for (let i = 0; i < inputs.length; ++i) {
+      const x = Visualizer.#getNodeX(inputs, i, left, right)
+      ctx.beginPath()
+      ctx.arc(x, bottom, nodeRadius, 0, Math.PI * 2)
+      ctx.fillStyle = 'black'
+      ctx.fill()
+
+      ctx.beginPath()
+      ctx.arc(x, bottom, nodeRadius * 0.6, 0, Math.PI * 2)
+      ctx.fillStyle = getRGBA(inputs[i])
+      ctx.fill()
+    }
+
+    // draw top level of neurons
+    for (let i = 0; i < outputs.length; ++i) {
+      const x = Visualizer.#getNodeX(outputs, i, left, right)
+      ctx.beginPath()
+      ctx.arc(x, top, nodeRadius, 0, Math.PI * 2)
+      ctx.fillStyle = 'black'
+      ctx.fill()
+
+      ctx.beginPath()
+      ctx.arc(x, top, nodeRadius * 0.6, 0, Math.PI * 2)
+      ctx.fillStyle = getRGBA(outputs[i])
+      ctx.fill()
+
+      // draw biases as contours
+      ctx.beginPath()
+      ctx.lineWidth = 2
+      ctx.arc(x, top, nodeRadius * 0.8, 0, Math.PI * 2)
+      ctx.strokeStyle = getRGBA(biases[i])
+      ctx.setLineDash([3, 3])
+      ctx.stroke()
+      ctx.setLineDash([])
+    }
+
+
+  }
+}
