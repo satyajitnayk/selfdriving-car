00c62955538ef58e273e1911422cf9152693a02d
satyajit nayak <satyajitn15@gmail.com>
2024-02-18 21:14:53 +0530

    add parallelization by creating many cars & more
    * add fitness function for car to choose best car(as of now straight road so car with minY).
    * save best car brain to localstorage.

--- car.js
+++ car.js
@@ -126,7 +126,7 @@ class Car {
     }
   }
 
-  draw(ctx, color) {
+  draw(ctx, color, drawSensor = false) {
     if (this.damaged) {
       ctx.fillStyle = 'gray'
     } else {
@@ -139,7 +139,7 @@ class Car {
     }
     ctx.fill()
 
-    if (this.sensor) {
+    if (this.sensor && drawSensor) {
       this.sensor.draw(ctx)
     }
   }
--- script.js
+++ script.js
@@ -8,37 +8,77 @@ const carCtx = carCanvas.getContext('2d')
 const networkCtx = networkCanvas.getContext('2d')
 
 const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9)
-const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI")
+
+const N = 100
+const cars = generateCars(N)
+let bestCar = cars[0]
+if (localStorage.getItem('bestBrain')) {
+  bestCar = JSON.parse(localStorage.getItem('bestBrain'))
+}
+
 const traffic = [
-  new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2)
+  new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(0), -300, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(2), -100, 30, 50, "DUMMY", 2),
 ];
 
 animate();
+
+function save() {
+  localStorage.setItem('bestBrain', JSON.stringify(bestCar.brain))
+}
+
+function discard() {
+  localStorage.removeItem('bestBrain')
+}
+
+function generateCars(N) {
+  const cars = []
+  for (let i = 1; i <= N; ++i) {
+    cars.push(new Car(road.getLaneCenter(1), 100, 30, 50, 'AI'))
+  }
+  return cars
+}
 
 function animate(time) {
   for (let i = 0; i < traffic.length; ++i) {
     traffic[i].update(road.borders, [])
   }
-  car.update(road.borders, traffic)
+
+  for (let i = 0; i < cars.length; ++i) {
+    cars[i].update(road.borders, traffic)
+  }
+
+  // car with min y, the one which one chasing the traffic
+  // using the below code as fitness function for best car
+  const minY = Math.min(...cars.map(car => car.y))
+  bestCar = cars.find(car => car.y === minY);
 
   carCanvas.height = window.innerHeight
   networkCanvas.height = window.innerHeight
 
   // make it look like a camera is following the car
   carCtx.save()
-  carCtx.translate(0, -car.y + carCanvas.height * 0.7)
+  carCtx.translate(0, -bestCar.y + carCanvas.height * 0.7)
 
   road.draw(carCtx)
   for (let i = 0; i < traffic.length; ++i) {
     traffic[i].draw(carCtx, 'red')
   }
-  car.draw(carCtx, 'blue')
+
+  carCtx.globalAlpha = 0.2
+  for (let i = 0; i < cars.length; ++i) {
+    cars[i].draw(carCtx, 'blue')
+  }
+  carCtx.globalAlpha = 1
+  bestCar.draw(carCtx, 'blue', true)
+
 
   carCtx.restore()
 
 
   // animate line dash using time param sent by requestAnimationFrame
   networkCtx.lineDashOffset = -time / 50
-  Visualizer.drawNetwork(networkCtx, car.brain)
+  Visualizer.drawNetwork(networkCtx, bestCar.brain)
   requestAnimationFrame(animate)
 }
